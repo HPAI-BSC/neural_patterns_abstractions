006feb21919b67cc78b0ef0cb415e34c26e22067
Unknown <raquelpa93@gmail.com>
2018-07-03 17:15:28 +0200

    Step-2 code finished
    Needs testing and documentation.

--- step-2/code/generate_dataset_2_class.py
+++ step-2/code/generate_dataset_2_class.py
@@ -1,21 +1,48 @@
 import os
+import numpy as np
+from datetime import timedelta
+import time
+
+
+# global paths
+tiramisu_base_dir = '/gpfs/projects/bsc28/tiramisu_semantic_transfer/'
+base_imgs_path = tiramisu_base_dir + 'imgs/'
 
 data_path = '/gpfs/projects/bsc28/DATASETS/Imagenet_val50k/ILSVRC_12_val'
-data_labels = '/gpfs/projects/bsc28/DATASETS/Imagenet_val50k/imagenet2012_val_synset_codes.txt'
-first_class_images = '/gpfs/projects/bsc28/bsc28535/tiramisu_3.0_dario_scripts/nltk_stuff/mammal_images.txt'
-source_target_path = '/gpfs/projects/bsc28/bsc28535/tiramisu_3.0_dario/imgs/imagenet2012_mam/train'
 
-#Store image names corresponding to living things
-liv_img = []
-with open(first_class_images,'r') as f:
-    for l in f:
-        liv_img.append(l.strip())
+imgs = np.load('/gpfs/projects/bsc28/DATASETS/Imagenet_val50k/imagenet2012_val_synset_codes_as_array.npz')['imgs']
+synsets = np.load('/gpfs/projects/bsc28/DATASETS/Imagenet_val50k/imagenet2012_val_synset_codes_as_array.npz')['ss']
 
-with open(data_labels,'r') as f:
-    for l in f:
-        img,synset = l.strip().split(' ')
-        #Create the symlink
-        if img in liv_img:
-            os.symlink(os.path.join(data_path,img),os.path.join(source_target_path,'mammal',img))
-        else:
-            os.symlink(os.path.join(data_path,img),os.path.join(source_target_path,'nomamm',img))
+
+def create_link(synset_data):
+
+	try:
+		os.makedirs(base_imgs_path + synset_data['name'] + '/train')
+	except:
+		print('peta')
+
+	source_target_path = base_imgs_path + synset_data['name'] + '/train'
+
+	for i in range(len(imgs)):
+		img, synset = imgs[i] , synsets[i]
+		if img in synset_data['imgs']:
+			os.symlink(os.path.join(data_path, img), os.path.join(source_target_path, synset_data['name'], img))
+		else:
+			os.symlink(os.path.join(data_path, img), os.path.join(source_target_path, 'no_' + synset_data['name'], img))
+
+
+def create_folders(image_file_paths):
+	synset_files = list(os.walk(image_file_paths))[0][2]
+	for file in synset_files:
+		ss_data = np.load(image_file_paths + file)
+		create_link(ss_data)
+
+def main():
+	image_file_paths = '/gpfs/projects/bsc28/tiramisu_semantic_transfer/synset_partitions/'
+	create_folders(image_file_paths)
+
+
+if __name__ == '__main__':
+	init = time.time()
+	main()
+	print('time:', timedelta(seconds=time.time() - init))
