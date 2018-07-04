8dcbc7a207e72552cbce100eff0ed0521a2e6d50
Unknown <raquelpa93@gmail.com>
2018-07-04 16:06:13 +0200

    The code to make the folder works
    Todo: documentate it

--- step-2/code/generate_dataset_2_class_55_ss.py
+++ step-2/code/generate_dataset_2_class_55_ss.py
@@ -3,7 +3,6 @@ import numpy as np
 from datetime import timedelta
 import time
 
-
 # global paths
 tiramisu_base_dir = '/gpfs/projects/bsc28/tiramisu_semantic_transfer/'
 base_imgs_path = tiramisu_base_dir + 'imgs/'
@@ -15,27 +14,51 @@ synsets = np.load('/gpfs/projects/bsc28/DATASETS/Imagenet_val50k/imagenet2012_va
 
 
 def create_link(synset_data):
+	_path = base_imgs_path + str(synset_data['name']) + '/train/'
+	try:
+		os.makedirs(_path)
+	except:
+		pass
+
+	source_target_path = _path
+
+	ss_img_path = os.path.join(source_target_path, str(synset_data['name']))
+	no_ss_img_path = os.path.join(source_target_path, 'no_' + str(synset_data['name']))
 
 	try:
-		os.makedirs(base_imgs_path + synset_data['name'] + '/train')
+		os.makedirs(ss_img_path)
 	except:
-		print('peta')
-
-	source_target_path = base_imgs_path + synset_data['name'] + '/train'
+		pass
+	try:
+		os.makedirs(no_ss_img_path)
+	except:
+		pass
 
 	for i in range(len(imgs)):
-		img, synset = imgs[i] , synsets[i]
+		img, synset = imgs[i], synsets[i]
 		if img in synset_data['imgs']:
-			os.symlink(os.path.join(data_path, img), os.path.join(source_target_path, synset_data['name'], img))
+			try:
+				os.symlink(os.path.join(data_path, img),
+				           os.path.join(source_target_path, str(synset_data['name']), img))
+			except:
+				pass
 		else:
-			os.symlink(os.path.join(data_path, img), os.path.join(source_target_path, 'no_' + synset_data['name'], img))
+			try:
+				os.symlink(os.path.join(data_path, img),
+				           os.path.join(source_target_path, 'no_' + str(synset_data['name']), img))
+			except:
+				pass
 
 
 def create_folders(image_file_paths):
 	synset_files = list(os.walk(image_file_paths))[0][2]
-	for file in synset_files:
+	# print(synset_files)
+	for file in synset_files[26:]:
 		ss_data = np.load(image_file_paths + file)
+		print(ss_data['name'])
 		create_link(ss_data)
+	print('links created :D ')
+
 
 def main():
 	image_file_paths = '/gpfs/projects/bsc28/tiramisu_semantic_transfer/synset_partitions/'
