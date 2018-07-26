7d538421419b3b408b5fd32436a61e299d794b61
Raquel <raquelpa93@gmail.com>
2018-07-26 15:22:12 +0200

    changed tabs to spaces on step3

--- step-3/driver_extractor_second_half.py
+++ step-3/driver_extractor_second_half.py
@@ -5,65 +5,64 @@ from tiramisu.tensorflow.core.backend import read_embeddings
 
 
 def test(model_name, dataset, embedding_types):
-	layers = launcher.get_layers(model_name)
-	datasets = launcher.get_datasets()
-	models = launcher.get_nns()
+    layers = launcher.get_layers(model_name)
+    datasets = launcher.get_datasets()
+    models = launcher.get_nns()
 
-	if model_name not in models:
-		print("Model \"{}\" is not available".format(model_name))
-		return -1
+    if model_name not in models:
+        print("Model \"{}\" is not available".format(model_name))
+        return -1
 
-	if dataset not in datasets:
-		print("Dataset \"{}\" is not available".format(dataset))
-		return -1
+    if dataset not in datasets:
+        print("Dataset \"{}\" is not available".format(dataset))
+        return -1
 
-	layers = [l[0] for l in layers]
-	job_id = launcher.run_experiment(
-		model_name=model_name,
-		dataset_name=dataset,
-		operations=None,
-		embedding_types=embedding_types
-	)
+    layers = [l[0] for l in layers]
+    job_id = launcher.run_experiment(
+        model_name=model_name,
+        dataset_name=dataset,
+        operations=None,
+        embedding_types=embedding_types
+    )
 
-	return job_id
-
+    return job_id
 
 
 def main():
-	nns = launcher.get_nns()
-	datasets = launcher.get_datasets()
+    nns = launcher.get_nns()
+    datasets = launcher.get_datasets()
 
-	print("Available models:", nns)
-	print("  Layer breakdown:")
+    print("Available models:", nns)
+    print("  Layer breakdown:")
 
-	for nn in nns:
-		print("   {}:".format(nn))
-		layers = launcher.get_layers(nn)
+    for nn in nns:
+        print("   {}:".format(nn))
+        layers = launcher.get_layers(nn)
 
-		for l in layers:
-			print("     ", l)
+        for l in layers:
+            print("     ", l)
 
-	print("Available datasets:", datasets)
+    print("Available datasets:", datasets)
 
-	# Second half of the datasets
-	datasets = [ 'musical_instrument', 'furniture', 'commodity', 'wheeled_vehicle', 'artifact', 'conveyance',
-	            'domestic_animal', 'primate', 'working_dog', 'object', 'garment', 'bird', 'whole', 'instrument',
-	            'substance', 'matter', 'furnishing', 'container', 'instrumentality', 'diapsid', 'produce',
-	            'protective_covering', 'device', 'placental']
+    # Second half of the datasets
+    datasets = ['musical_instrument', 'furniture', 'commodity', 'wheeled_vehicle', 'artifact', 'conveyance',
+                'domestic_animal', 'primate', 'working_dog', 'object', 'garment', 'bird', 'whole', 'instrument',
+                'substance', 'matter', 'furnishing', 'container', 'instrumentality', 'diapsid', 'produce',
+                'protective_covering', 'device', 'placental']
 
-	model_name = "vgg16_imagenet"
-	for dataset in datasets:
-		ret = test(model_name, dataset, "FN")
-		if None in ret:
-			print("Job crashed!")
-			return
+    model_name = "vgg16_imagenet"
+    for dataset in datasets:
+        ret = test(model_name, dataset, "FN")
+        if None in ret:
+            print("Job crashed!")
+            return
 
-		embeddings_shape, duration, location = ret
+        embeddings_shape, duration, location = ret
 
-		print("Time spent extracting", duration, " (seconds)")
-		print("Shapes:", embeddings_shape)
-		print("output:", location)
+        print("Time spent extracting", duration, " (seconds)")
+        print("Shapes:", embeddings_shape)
+        print("output:", location)
 
 
 if __name__ == "__main__":
-	main()
+    main()
